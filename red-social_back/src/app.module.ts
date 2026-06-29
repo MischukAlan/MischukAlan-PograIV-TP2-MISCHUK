19b46e8972de2e036ab0ed8f33a9ce6fbd9c653c
MischukAlan <138259789+MischukAlan@users.noreply.github.com>
2026-06-28 21:38:33 -0300

    .env

--- red-social_back/src/app.module.ts
+++ red-social_back/src/app.module.ts
@@ -1,25 +1,35 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsuariosModule } from './usuarios/usuarios.module';
-import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
 import { PublicacionesModule } from './publicaciones/publicaciones.module';
 import { ComentariosModule } from './comentarios/comentarios.module';
 import { EstadisticasModule } from './estadisticas/estadisticas.module';
 
 @Module({
-  controllers: [AppController],
-  providers: [AppService],
   imports: [
-    MongooseModule.forRoot(
-      'mongodb+srv://MischukProgra:MischukAlan@clusterprogra4.qozsmcv.mongodb.net/nombre_de_tu_db?appName=ClusterProgra4'
-    ),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    MongooseModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'),
+      }),
+    }),
+
     UsuariosModule,
     AuthModule,
     PublicacionesModule,
     ComentariosModule,
     EstadisticasModule,
   ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
